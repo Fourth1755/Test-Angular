4f5d94c6d30a260243e4dc2a15b2b731167c897b
fourth <fourthe.hero@gmail.com>
2022-11-30 14:04:01 +0700

    Create book

---
+++ server/src/book/entities/book.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Book {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    name: string;
+  
+    @Column()
+    image: string;
+
+    @Column()
+    score: number;
+  
+    @Column({ default: true })
+    isActive: boolean;
+}
