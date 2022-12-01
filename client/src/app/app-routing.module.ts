4842acbe5081a1d933d0294745b05fb0ce328580
fourth <fourthe.hero@gmail.com>
2022-12-01 18:40:07 +0700

    Update app-routing.module.ts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { BookListComponent } from './books/book-list/book-list.component';
 
 const routes: Routes = [
   {path:'',pathMatch:'full',redirectTo: 'add-book'},
+  {path:'book-list',component:BookListComponent},
 ];
 
 @NgModule({
