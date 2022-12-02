52fd2029902e8473b169178331d9823a08f8d792
fourth <fourthe.hero@gmail.com>
2022-12-02 14:52:35 +0700

    Update book-list.module.ts

--- client/src/app/books/book-list/book-list.module.ts
+++ client/src/app/books/book-list/book-list.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookService } from 'src/app/_services/sv-book/book.service';
+import { BookListComponent } from './book-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BookListComponent,
+  ],
   providers:[BookService],
   imports: [
     CommonModule
-  ]
+  ],
+  exports:[
+    BookListComponent
+  ],
 })
 export class BookListModule { }
