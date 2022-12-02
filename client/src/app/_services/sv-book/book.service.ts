97c151e94eb430b4e6f44b38c3f9a280be155464
fourth <fourthe.hero@gmail.com>
2022-12-02 17:10:53 +0700

    Update book.service.ts

--- client/src/app/_services/sv-book/book.service.ts
+++ client/src/app/_services/sv-book/book.service.ts
@@ -7,5 +7,21 @@ export class BookService {
   getBook(){
     return Data
   }
+  getBookById(){
+    return  {
+      "id":1,
+      "name":"Sword art online",
+      "year":2012,
+      "studios":"A-1 Pictures",
+      "seasonal":"Summer",
+      "episodes":25,
+      "image":"https://cdn.myanimelist.net/images/anime/11/39717.jpg",
+      "trailer":"https://www.youtube.com/embed/6ohYYtxfDCg",
+      "trailer_start":"",
+      "wallpaper":"https://wallpaperaccess.com/full/1122002.jpg",
+      "duration":"23 min. per ep.",
+      "score":7.2
+    }
+  }
   constructor() { }
 }
