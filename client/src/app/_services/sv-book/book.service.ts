68993c89072ad8ccfd27e33a2b5c3e118a70f26e
fourth <fourthe.hero@gmail.com>
2022-12-02 14:52:43 +0700

    Update book.service.ts

--- client/src/app/_services/sv-book/book.service.ts
+++ client/src/app/_services/sv-book/book.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-
+import Data from '../../../assets/json/db.json'
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
-
+  getBook(){
+    return Data
+  }
   constructor() { }
 }
