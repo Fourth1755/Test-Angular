83f477cf504ba3a404ad548969581ec1670e10ee
fourth <fourthe.hero@gmail.com>
2022-12-06 16:33:07 +0700

    Update add-book.module.ts

--- client/src/app/books/add-book/add-book.module.ts
+++ client/src/app/books/add-book/add-book.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AddBookRoutingModule } from './add-book-routing.module';
 import { AddBookComponent } from './add-book.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { BookService } from 'src/app/_services/sv-book/book.service';
 
 @NgModule({
   declarations: [
     AddBookComponent
   ],
+  providers:[BookService],
   imports: [
     CommonModule,
     AddBookRoutingModule,
