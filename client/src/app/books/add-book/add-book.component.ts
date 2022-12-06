a64162e4e9363794026cf0cf66e745d19d77a8ea
fourth <fourthe.hero@gmail.com>
2022-12-06 17:32:47 +0700

    Update add-book.component.ts

--- client/src/app/books/add-book/add-book.component.ts
+++ client/src/app/books/add-book/add-book.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { BookService } from 'src/app/_services/sv-book/book.service';
 
 @Component({
@@ -8,20 +8,24 @@ import { BookService } from 'src/app/_services/sv-book/book.service';
   styleUrls: ['./add-book.component.css']
 })
 export class AddBookComponent {
-
-  
-
   constructor(
     private bookService: BookService,
     private formBuilder: FormBuilder,
   ) {}
-  checkoutForm = this.formBuilder.group({
-    name: '',
-    address: ''
+    checkoutForm = this.formBuilder.group({
+    name: "",
+    image: "",
+    score: 0,
+    year: "",
   });
+  res=""
   onSubmit(): void {
     // Process checkout data here
     //this.items = this.cartService.clearCart();
+    // this.bookService.addBook({this.checkoutForm.value})
+    //     .subscribe(response => {
+    //       this.res = response;
+    //     });
     console.warn('Your order has been submitted', this.checkoutForm.value);
     this.checkoutForm.reset();
   }
