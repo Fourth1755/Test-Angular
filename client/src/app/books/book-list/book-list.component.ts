fc0a8771b411d17ce0250c5ca795a01d38de1ccf
fourth <fourthe.hero@gmail.com>
2022-12-07 10:44:11 +0700

    Update book-list.component.ts

--- client/src/app/books/book-list/book-list.component.ts
+++ client/src/app/books/book-list/book-list.component.ts
@@ -15,7 +15,7 @@ interface Book{
 export class BookListComponent {
 
   constructor(private bookService:BookService){}
-  books: any
+  books: any = []
   ngOnInit() {
     this.bookService.getBook()
       .subscribe(response => {
@@ -24,7 +24,16 @@ export class BookListComponent {
   }
   searchText = '';
   isShowing : boolean = true
-
+  handerDelete(id:any,i:any){
+    console.log(id)
+    if(window.confirm('Do you want to delete this book')){
+      this.bookService.deleteBook(id).subscribe(response => {
+        this.books.splice(i,1)
+      });
+    }
+    
+    
+  }
   handerInput(event:any){
     console.log(event.target.value)
   }
