c656c36efef3ccfff0c65990bf2e89a516c76536
fourth <fourthe.hero@gmail.com>
2022-12-09 09:14:26 +0700

    Update book-detail.module.ts

--- client/src/app/books/book-detail/book-detail.module.ts
+++ client/src/app/books/book-detail/book-detail.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import { BookDetailComponent } from './book-detail.component';
 import { BookDetailRoutingModule } from './book-detail-routing.module';
 import { BookService } from 'src/app/_services/sv-book/book.service';
-
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [BookDetailComponent],
   providers:[BookService],
   imports: [
     CommonModule,
-    BookDetailRoutingModule
+    BookDetailRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   exports:[
-    BookDetailComponent
+    BookDetailComponent,
+   
   ],
 })
 export class BookDetailModule { }
